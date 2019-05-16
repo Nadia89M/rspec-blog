b452bec1331920600d0fcb720659c6bf4998437c
Nadia <nadia.mohamed@solirius.com>
2019-05-16 13:27:51 +0100

    Fix broken delete articles spec

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -1,15 +1,15 @@
+require "rails_helper"
 RSpec.feature "Listing Articles" do
     before do
         john = User.create(email: "john@example.com", password: "password")
         login_as(john)
         @article1 = Article.create(title: "The first article",
-        body: "Lorem ipsum dolor sit amet, consectetur.", user: joh) 
+        body: "Lorem ipsum dolor sit amet, consectetur.", user: john) 
         @article2 = Article.create(title: "The second article",
-        body: "Pellentesque ac ligula in tellus feugiat.", user: joh) 
+        body: "Pellentesque ac ligula in tellus feugiat.", user: john) 
     end
     
     scenario "A user lists all articles" do
-        
         visit "/"
         expect(page).to have_content(@article1.title) 
         expect(page).to have_content(@article1.body) 
